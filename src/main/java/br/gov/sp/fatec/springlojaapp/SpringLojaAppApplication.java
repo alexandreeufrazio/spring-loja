15e0368bee7376923b88d246eebf6d40b43e4256
alexandreeufrazio <alee.ram@hotmail.com>
2021-08-26 22:41:58 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/springlojaapp/SpringLojaAppApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springlojaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLojaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLojaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/springlojaapp/SpringLojaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springlojaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLojaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
