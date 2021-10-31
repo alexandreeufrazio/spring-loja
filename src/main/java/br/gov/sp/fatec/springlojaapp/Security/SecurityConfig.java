5295c53e65594467aa8349f3683347634c6123b6
alexandreeufrazio <alee.ram@hotmail.com>
2021-10-31 16:33:22 +0000

    SecurityConfig 31102021

--- src/main/java/br/gov/sp/fatec/springlojaapp/Security/SecurityConfig.java
+++ src/main/java/br/gov/sp/fatec/springlojaapp/Security/SecurityConfig.java
@@ -1,10 +1,13 @@
 package br.gov.sp.fatec.springlojaapp.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -18,6 +21,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
-    
-    
+    @Bean
+    public PasswordEncoder passwordEncoderBean() {
+	    return new BCryptPasswordEncoder();
+    }  
 }
