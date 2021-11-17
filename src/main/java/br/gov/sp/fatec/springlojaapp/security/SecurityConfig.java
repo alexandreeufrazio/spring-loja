f7084ce56694960278a0728a5a6a993ea226f68a
alexandreeufrazio <alee.ram@hotmail.com>
2021-11-17 00:03:41 +0000

    security-16-11-2011-1

--- src/main/java/br/gov/sp/fatec/springlojaapp/security/SecurityConfig.java
+++ src/main/java/br/gov/sp/fatec/springlojaapp/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package br.gov.sp.fatec.springlojaapp.Security;
+package br.gov.sp.fatec.springlojaapp.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
